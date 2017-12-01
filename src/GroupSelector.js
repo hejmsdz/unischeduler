6295b5514992d286198610155b46ec17141124dd
Mikołaj Rozwadowski <mikolaj.rozwadowski@outlook.com>
2017-12-01 23:02:57 +0100

    Display a table

--- src/GroupSelector.js
+++ src/GroupSelector.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 
 class GroupSelector extends Component {
+  constructor() {
+    super();
+    this.state = {courses: {}};
+  }
+
+  componentWillMount() {
+    fetch('/data/groups.json')
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({courses: data});
+      });
+  }
+
   render() {
     return (
-      <span>Select your groups.</span>
+      <table>
+        <thead>
+          <tr>
+            <th>Course</th>
+            <th>Theory group</th>
+            <th>Lab group</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(this.state.courses).map(([name, [theoryGroups, labGroups]]) => (
+          <tr key={name}>
+            <td>{name}</td>
+            <td>{Object.keys(theoryGroups).join('; ')}</td>
+            <td>{Object.keys(labGroups).join('; ')}</td>
+          </tr>
+          ))}
+        </tbody>
+      </table>
     );
   }
 }
