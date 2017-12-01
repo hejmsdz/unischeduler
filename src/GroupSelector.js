1833fc70483af63969bf731bb6bd43b614f5e5f3
Mikołaj Rozwadowski <mikolaj.rozwadowski@outlook.com>
2017-12-01 22:47:50 +0100

    First component

---
+++ src/GroupSelector.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class GroupSelector extends Component {
+  render() {
+    return (
+      <span>Select your groups.</span>
+    );
+  }
+}
+
+export default GroupSelector;
