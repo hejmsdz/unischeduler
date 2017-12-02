1375129309e20149a55862b2afa040fcc94c64cf
Mikołaj Rozwadowski <mikolaj.rozwadowski@outlook.com>
2017-12-02 11:20:45 +0100

    Add SCSS preprocessing

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -151,6 +151,12 @@ module.exports = {
               cacheDirectory: true,
             },
           },
+          // sass
+          {
+            test: /\.scss$/,
+            include: paths.appSrc,
+            use: ['style-loader','css-loader', 'sass-loader'],
+          },
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
           // "style" loader turns CSS into JS modules that inject <style> tags.
