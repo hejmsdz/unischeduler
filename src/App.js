22991a3bafb9932b921055bb5cb13558658ab986
Mikołaj Rozwadowski <mikolaj.rozwadowski@outlook.com>
2017-12-03 13:52:00 +0100

    Remove unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import GroupSelector from './GroupSelector';
 import SvgSchedule from './SvgSchedule';
